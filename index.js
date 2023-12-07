4315821b6b4851c9c2aa6dec75b18fbc420517d8
DiiKhanh <duykhanh.030803@gmail.com>
2023-12-07 14:56:01 +0700

    fix structure v2

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
-app.use(express.static(path.join(__dirname, 'public')))
+// app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/order', order)
 app.use('/send', email)
